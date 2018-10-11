dc153553cfd0c03958deef978ca1e174a91a50b6
Valentina Kononenko <valentina@kononenko.net>
2018-10-11 14:02:18 -0400

    done

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,22 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+    private int mpg;
+    private double gallons = 0;
+
+    public Automobile(int m) {
+        mpg = m;
+    }
+
+    public void fillUp(double fill) {
+        gallons = gallons + fill;
+    }
+
+    public void takeTrip(double take) {
+        gallons = gallons - take / mpg;
+    }
+
+    public double reportFuel() {
+        return gallons;
+    }
 }
